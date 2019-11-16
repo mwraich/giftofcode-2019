848b878b163cee3ab8d1500657e3b9c5fbc6302c
Mandeep <mkaur@pagerduty.com>
2019-11-16 14:21:36 -0500

    mend

--- src/containers/FosterHomes/actions/index.js
+++ src/containers/FosterHomes/actions/index.js
@@ -25,7 +25,6 @@ export function fetchFosterHomes() {
             error => dispatch(handleError(error)),
         )
         .then((data) => {
-            console.log(data),
             dispatch(receivedFosterHomes(data.fosterHomes));
         });
     };

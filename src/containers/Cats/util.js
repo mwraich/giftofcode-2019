a4e5aa5e39271153c2acdcd881a15bc4c09f424a
Mandeep <mkaur@pagerduty.com>
2019-11-16 14:02:00 -0500

    Make https

--- src/containers/Cats/util.js
+++ src/containers/Cats/util.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const getCats = async => axios({
   method: 'GET',
-  url: 'http://cmsback-sxbbrr2s2a-uc.a.run.app/cat'
+  url: 'https://cmsback-sxbbrr2s2a-uc.a.run.app/cat'
 });

afd278d56d97bbcdb9bbdb41ee4d64c1f0fa7c5d
auredufour <aurelie.dufour@tophatmonocle.com>
2019-11-16 11:40:20 -0500

    Add action/reducer for foster homes

--- src/containers/FosterHomes/reducer/index.js
+++ src/containers/FosterHomes/reducer/index.js
@@ -1,11 +1,29 @@
 import types from '../actions/types';
 
 const defaultState = {
-  cats: []
+    isFetching: false,
+    error: [],
+    fosterHomes: []
 };
 
 export default function catReducer(state = defaultState, action) {
     switch(action.type) {
+        case types.HANDLE_ERROR:
+            return {
+                ...state,
+                error: action.error,
+            }
+        case types.REQUEST_FOSTER_HOMES:
+            return {
+                ...state,
+                isFetching: true
+            }
+        case types.RECEIVE_FOSTER_HOMES:
+                return {
+                    ...state,
+                    isFetching: false,
+                    fosterHomes: action.fosterHomes
+                }
         default:
             return state;
     }
